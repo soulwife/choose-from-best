b1c18b3850b9174adb499ece272c6c0c60fb753c
Anastasiia <mysterycconstellation@gmail.com>
2018-03-01 14:50:23 +0200

    implemented: display best random entities for each category in bootstrap carousel with images

--- src/SoulFamily/BestEntityBundle/Controller/AppController.php
+++ src/SoulFamily/BestEntityBundle/Controller/AppController.php
@@ -6,6 +6,11 @@ use SoulFamily\BestEntityBundle\Entity\Category;
 use SoulFamily\BestEntityBundle\Entity\EntityDescription;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class AppController extends Controller
 {
@@ -16,14 +21,67 @@ class AppController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $user = $this->getUser();
-        $categories = $em->getRepository(Category::class)->findAll();
-        $bestRandomChoices = [];
 
-        array_walk($categories, function($category) use ($em, &$bestRandomChoices) {
+
+        $categories = $em->getRepository(Category::class)->findAll();
+        $bestRandomChoices = $userEntities = [];
+        if ($user) {
+            $userEntities = $user->getBestEntities();
+        }
+
+        array_walk($categories, function($category) use ($em, &$bestRandomChoices, $userEntities) {
             $categoryEntities = $em->getRepository(EntityDescription::class)->findBy(['category' => $category]);
-            $bestRandomChoices[$category->getId()] = $categoryEntities ? $categoryEntities[array_rand($categoryEntities)] : null;
+            if ($categoryEntities) {
+                $categoryEntitiesWithoutUserEntities = array_diff($categoryEntities, $userEntities);
+                $bestRandomChoices[$category->getId()] = $categoryEntitiesWithoutUserEntities[array_rand($categoryEntitiesWithoutUserEntities)];
+            } else {
+                $bestRandomChoices[$category->getId()]  = [];
+            }
         });
 
         return $this->render('app/index.html.twig', ['bestRandomChoices' => $bestRandomChoices]);
+    }
+
+    /**
+     * @Route("/categories/{name}", name="category_entities")
+     * @Method("GET")
+     */
+    public function showEntitiesAction(Category $category)
+    {
+        $user = $this->getUser();
+        $userEntitiesIds = [];
+        if ($user) {
+            $userEntitiesIds = $user->getBestEntities()->map(function($entity)  {
+                return $entity->getId();
+            })->toArray();
+
+        }
+        return $this->render('app/category_entities.html.twig', ['category' => $category, 'userEntitiesIds' => $userEntitiesIds]);
+    }
+
+    /**
+     * @Route("/saveUserEntities", name="save_user_entities")
+     * @Method("POST")
+     * @Security("is_granted('IS_AUTHENTICATED_REMEMBERED')")
+     * @param Request $request
+     * @return Response;
+     */
+    public function saveUserEntitiesAction(Request $request)
+    {
+        $user = $this->getUser();
+        if ($request->isXMLHttpRequest()) {
+            $content = $request->getContent();
+            if (!empty($content)) {
+                $em = $this->getDoctrine()->getManager();
+                $entitiesIds = explode(',', json_decode($content, true));
+                $entities = $em->getRepository(EntityDescription::class)->findBy(['id' => $entitiesIds]);
+                $user->setBestEntities($entities);
+                $em->persist($user);
+                $em->flush();
+            }
+
+            return new JsonResponse(array('data' => ''));
+        }
+        return new Response('Error!', 400);
     }
 }
--- web/js/app.js
+++ web/js/app.js
@@ -1,3 +1,32 @@
-/**
- * Created by constellation on 2/21/18.
- */
+
+$(document).ready(function(){
+    $(".edit-list-btn").click(function(){
+        $(".read-list-btn").toggle();
+        $(".entities-list-checkbox").toggle();
+    });
+
+    $(".entities-list-checkbox").change(function() {
+        this.checked ? $(this).parent().addClass('disabled') : $(this).parent().removeClass('disabled');
+    });
+
+    $(".read-list-btn").on('click', function() {
+        var checkedItems = $('.entities-list-checkbox:checkbox:checked').map(function() {
+            return this.value;
+        }).get();
+
+        $.ajax({
+            type: 'post',
+            url: $(this).data('url'),
+            data: JSON.stringify(checkedItems.join(',')),
+            dataType: 'json',
+            beforeSend: function () {
+                $('#loading').show();
+            },
+            success: function (json) {
+                $('#loading').hide();
+            }
+        });
+    });
+
+});
+
